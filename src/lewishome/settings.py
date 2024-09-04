60adddb561b2276b33b4caa3fa0c9a62748153c3
lewisnjue <lewiskinyuanjue.ke@gmail.com>
2024-09-04 11:41:55 +0300

    updated connection to database

--- src/lewishome/settings.py
+++ src/lewishome/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from os import getenv
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -83,17 +84,11 @@ WSGI_APPLICATION = 'lewishome.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 DATABASES = {
-  'default': {
-    'ENGINE': 'django.db.backends.postgresql',
-    'NAME': getenv('PGDATABASE'),
-    'USER': getenv('PGUSER'),
-    'PASSWORD': getenv('PGPASSWORD'),
-    'HOST': getenv('PGHOST'),
-    'PORT': getenv('PGPORT', 5432),
-    'OPTIONS': {
-      'sslmode': 'require',
-    },
-  }
+  'default': dj_database_url.config(
+      default=getenv('PGURL'),
+      conn_max_age=6000,
+      conn_health_checks=True
+  )
 }
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
